45281f81fdf082fae9449eba144bb5c9bb1fa654
mughees khatri <128294671+mohtashimkhatri@users.noreply.github.com>
2023-09-07 01:31:39 -0700

    intial commit

---
+++ app.js
@@ -0,0 +1,87 @@
+// chapter 1
+// Q1
+alert("Last Name")
+// Q2
+alert("First Name")
+// Q3
+alert("Email")
+// Q4
+alert("Password")
+//     chapter 2 start    // 
+// Q1
+var username = "Ubaid";
+var Password = ("Raza");
+alert(username + Password );
+// Q2
+var username = "zain";
+var Password = (" ul abidin");
+alert(username + Password );
+// Q3
+var username = "Mohtashim";
+var Password = ("Raza");
+alert(username + Password );
+// Q4
+var username = "Hamid";
+var Password = ("Raza");
+alert(username + Password );
+// Q5
+var username = "Mugsese";
+var Password = ("Raza");
+alert(username + Password );
+//chaoter 2 end//
+//chapter 3 start//
+// Q1
+ var originalNum = 23;
+ var numToBeAdded = 7;
+ var newNum = originalNum + numToBeAdded;
+ alert(newNum)
+//  Q2
+var originalNum = 25;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+alert(newNum)
+// Q3
+var originalNum = 20;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+alert(newNum)
+// Q4
+var originalNum = 21;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+alert(newNum)
+// Q5
+var originalNum = 55;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+alert(newNum)
+//chapter 3 end//
+
+//   chapter 5 stat //
+// Q1
+var start = (2+2)
+alert(2+20)
+// Q2
+var start = (52*2)
+alert(50*2)
+// Q3
+var start = (98+2)
+alert(98+2)
+// Q4
+var start = (30-2)
+alert(30-2)
+// chapter 5 complet //
+
+ //chaptter 4 start //
+//  Q1
+//  Legal Names
+var $myVariable ="Learn Javascript"
+var _myprofile = "This is my profile"
+var Num03332235385 = "mynumber"
+ Q2 
+ Illegal
+var 0515855814num = "my num"
+ var !myVariable = "This is variable"
+ var @MyAncartag = "This is ancartags"
+//   chaoter 4 end  //
+
